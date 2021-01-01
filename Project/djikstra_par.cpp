c1e708e3ea9b1b72629c7d0e99782cd1b668ce3c
Harlock <harlockofficial.github@mail.com>
2021-01-01 15:46:24 +0100

    boost mpi version, bug in deserialization after recv

--- Project/djikstra_par.cpp
+++ Project/djikstra_par.cpp
@@ -22,41 +22,97 @@ const int NUM_OF_WORKERS = THREAD_COUNT-2;
 #define WORKER_TO_COLLECTOR_TAG 3
 #define COLLECTOR_TO_SPLITTER_TAG 4
 
-class Node;
-
-class Edge{
-    public:
-        long long int weight = LLONG_MAX;
-        Node n[1] = nullptr;
-    private:
-        friend class boost::serialization::access;
-        template <typename Archive>
-        void serialize(Archive& ar, const unsigned int version){
-            ar & weight;
-            ar & n;
-        }
-};
+class Edge;
 
 class Node{
     public:
         std::vector<Edge> in, out; 
         std::string name = "";   //unique identifier of a Node
         unsigned long long int distance = ULLONG_MAX; //default infinity
-        Node nearestPrev[1] = nullptr;   //default NULL
-    private:
+        Node* nearestPrev;   //default NULL
+        void fix_before_sending() const {
+            if(nearestPrev == NULL){
+                return;
+            }
+            //will give error
+            nearestPrev->in = std::vector<Edge>();
+            nearestPrev->out= std::vector<Edge>();
+            //nearestPrev->fix_before_sending();
+            nearestPrev->nearestPrev = nullptr;
+            //------------------------------ 
+            //std::cout<<"nearestPrev data ready to be serialized\n";
+        }
         friend class boost::serialization::access;
-        template <typename Archive>
-        void serialize(Archive& ar, const unsigned int version){
+        template <class Archive>
+        void save(Archive& ar, const unsigned int version) const {
+            fix_before_sending();
+            //std::cout<<"serialization start Node\n";
+            ar & in;
+            //std::cout<<"in serialization complete\n";
+            ar & out;
+            //std::cout<<"out serialization complete\n";
+            ar & name;
+            //std::cout<<"name serialization complete\n";
+            ar & distance;
+            //std::cout<<"distance serialization complete\n";
+            //the error might be that nearestPrev is changed during deserialization, 
+            //and the serialization fails
+            if(nearestPrev!=NULL){
+                nearestPrev->serialize(ar, version);
+            }else{
+                ar & nearestPrev;
+            }
+            //std::cout<<"nearestPrev serialization complete\n";
+        }
+        template<class Archive>
+        void load(Archive & ar, const unsigned int version){
             ar & in;
             ar & out;
             ar & name;
             ar & distance;
             ar & nearestPrev;
         }
+        BOOST_SERIALIZATION_SPLIT_MEMBER()
 };
 
-void djikstra_splitter(std::vector<Node> &start,
-            boost::mpi::communicator world){
+class Edge{
+    public:
+        long long int weight = LLONG_MAX;
+        Node* n;
+        void fix_before_sending() const {
+            if(n == NULL){ //impossible
+                return;
+            }
+            //will give error
+            n->in = std::vector<Edge>();
+            n->out= std::vector<Edge>();
+            n->nearestPrev = nullptr;
+            //------------------------------
+            //std::cout<<"n data ready to be serialized\n";
+        }
+        friend class boost::serialization::access;
+        template <class Archive>
+        void save(Archive& ar, const unsigned int version) const {
+            fix_before_sending();
+            //std::cout<<"serialization start Edge\n";
+            ar & weight;
+            //std::cout<<"serialized weight";
+            if(n != NULL){
+                n->serialize(ar, version);
+            }else{
+                ar & n;
+            }
+            //std::cout<<"serialization complete Edge\n";
+        }
+        template <class Archive>
+        void load(Archive& ar, const unsigned int version){
+            ar & weight;
+            ar & n;
+        }
+        BOOST_SERIALIZATION_SPLIT_MEMBER()
+};
+
+void djikstra_splitter(std::vector<Node> &start, boost::mpi::communicator world){
     std::queue<Node> queue;
     queue.push(start[0]);  //start enters the queue
     int free_worker_id;
@@ -64,31 +120,58 @@ void djikstra_splitter(std::vector<Node> &start,
         world.recv(boost::mpi::any_source, FREE_WORKER_TAG, free_worker_id);
         Node current = queue.front();    //get first node
         queue.pop(); //removes first node
+
+        //should solve serialization missing data
+        for(Node n: start){
+            if(current.name==n.name){
+                n.distance = current.distance;
+                current = n;
+                break;
+            }
+        }
+        // maybe
+        //current.fix_before_sending();
+        std::cout<<"assigning work to "<<free_worker_id<<"\n";
         world.send(free_worker_id, ASSIGN_WORK_TAG, &current);
-        
+        std::cout<<"in2\n";
         if(queue.empty()){
+            std::cout<<"empty queue 1\n";
             Node n;
             //receive from collector queue containing other nodes
             //blocking call (may create problems in the end)
             world.recv(COLLECTOR_ID, COLLECTOR_TO_SPLITTER_TAG, n);
+            std::cout<<"empty queue 2\n";
             //if n not null
             //impossible
             if(n.name == ""){
+                std::cout<<"exit 1\n";
                 break;
             }
-            queue.push(*n);
+            //should solve serialization missing data
+            for(Node tmp: start){
+                if(n.name==tmp.name){
+                    tmp.distance = n.distance;
+                    n = tmp;
+                    break;
+                }
+            }
+            // maybe
+            queue.push(n);
         }
     }
+    std::cout<<"exit2\n";
     Node final;
     final.name = "";
     for(int worker_id = SPLITTER_ID+1; worker_id<COLLECTOR_ID;++worker_id){
-        world.send(worker_id, ASSIGN_WORK_TAG, final);
+        //final.fix_before_sending();
+        world.isend(worker_id, ASSIGN_WORK_TAG, final);
     }
 }
 void djikstra_collector(boost::mpi::communicator world){
     Node n;
     while(true){
-        world.recv(boost::mpi::any_source, WORKER_TO_COLLECTOR_TAG, &n);
+        world.recv(boost::mpi::any_source, WORKER_TO_COLLECTOR_TAG, n);
+        //n.fix_before_sending();
         world.isend(SPLITTER_ID, COLLECTOR_TO_SPLITTER_TAG, &n);
         if(n.name == ""){
             break;
@@ -98,12 +181,16 @@ void djikstra_collector(boost::mpi::communicator world){
 void djikstra_worker(boost::mpi::communicator world){
     Node n;
     while(true){
-        //notify with blocking call that worker is free
+        //notify with potentially blocking call that worker is free
         world.send(SPLITTER_ID, FREE_WORKER_TAG, world.rank());
-        world.recv(SPLITTER_ID, ASSIGN_WORK_TAG, &n);
+        std::cout<<"waiting for work rank "<<world.rank()<<"\n";
+        //TODO fix deserialization problem length error, may be vector
+        world.recv(SPLITTER_ID, ASSIGN_WORK_TAG, n);
+        std::cout<<"got work rank "<<world.rank()<<"\n";
         //never happens, always receive front of queue
         if(n.name==""){
             if(world.rank()==COLLECTOR_ID-1){
+                //n.fix_before_sending();
                 world.send(COLLECTOR_ID, WORKER_TO_COLLECTOR_TAG, &n);
             }
             break;
@@ -114,8 +201,10 @@ void djikstra_worker(boost::mpi::communicator world){
             if(tmpDistance<n.out[i].n->distance){
                 n.out[i].n->distance=tmpDistance;
                 n.out[i].n->nearestPrev=&n;
+                //TODO fix nearestPrev might be reset
                 //non blocking call to send new node in queue
-                world.isend(COLLECTOR_ID, WORKER_TO_COLLECTOR_TAG, &(n.out[i].n));
+                //n.out[i].n->fix_before_sending();
+                world.isend(COLLECTOR_ID, WORKER_TO_COLLECTOR_TAG, n.out[i].n);
             }
         }
     }
@@ -162,7 +251,7 @@ int main(int argc, char* argv[]){
     }
 
     std::vector<Node> graph;
-    boost::mpi::environment env(argc, argv); //initialize the environment
+    boost::mpi::environment env; //initialize the environment
     boost::mpi::communicator world; //initialize communicator
     
     srand(123456);
@@ -186,32 +275,32 @@ int main(int argc, char* argv[]){
     //-------------------------------
 
     if(world.rank()==SPLITTER_ID){
+        auto funct_start = std::chrono::steady_clock::now();
         djikstra_splitter(graph, world);
+        auto funct_end = std::chrono::steady_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(funct_end - funct_start);
+        std::cout<<"Time required by djikstra_par: "<<duration.count()<<" milliseconds\n";
+        for(long unsigned int i = 0 ;i<graph.size();++i){
+            std::cout<<"node "<<i<<"\n"
+                <<graph[i].name<<" distance:"<<graph[i].distance<<"\n"
+                <<"\n--------------------------------------------------\n"
+                <<"out nodes: \n";
+            for(long unsigned int j = 0;j<graph[i].out.size();++j){
+                std::cout<<"\t\t"<<graph[i].out[j].weight<<" "<<graph[i].out[j].n->name<<"\n";
+            }
+            std::cout<<"\n--------------------------------------------------\n";
+            std::cout<<"in nodes: \n";
+            for(long unsigned int j = 0;j<graph[i].in.size();++j){
+                std::cout<<"\t\t"<<graph[i].in[j].weight<<" "<<graph[i].in[j].n->name<<"\n";
+            }
+            std::cout<<"\n--------------------------------------------------\n";
+        }
+        
     }else if(world.rank()==COLLECTOR_ID){
         djikstra_collector(world);
     }else{
         djikstra_worker(world);
     }
 
-    auto funct_start = std::chrono::steady_clock::now();
-    auto funct_end = std::chrono::steady_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(funct_end - funct_start);
-    std::cout<<"Time required by djikstra_par: "<<duration.count()<<" milliseconds\n";
-    for(long unsigned int i = 0 ;i<graph.size();++i){
-        std::cout<<"node "<<i<<"\n"
-            <<graph[i].name<<" distance:"<<graph[i].distance<<"\n"
-            <<"\n--------------------------------------------------\n"
-            <<"out nodes: \n";
-        for(long unsigned int j = 0;j<graph[i].out.size();++j){
-            std::cout<<"\t\t"<<graph[i].out[j].weight<<" "<<graph[i].out[j].n->name<<"\n";
-        }
-        std::cout<<"\n--------------------------------------------------\n";
-        std::cout<<"in nodes: \n";
-        for(long unsigned int j = 0;j<graph[i].in.size();++j){
-            std::cout<<"\t\t"<<graph[i].in[j].weight<<" "<<graph[i].in[j].n->name<<"\n";
-        }
-        std::cout<<"\n--------------------------------------------------\n";
-    }
-    
     return 0;
 }
