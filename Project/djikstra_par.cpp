9fd1af8099ac7d7f0b1981d01f1f1870b9d869bb
Harlock <harlockofficial.github@mail.com>
2021-01-02 15:17:04 +0100

    major changes still buggy
    deserialization problem in Edge, after data is received, deserialization fails, std::bad_alloc is thrown

--- Project/djikstra_par.cpp
+++ Project/djikstra_par.cpp
@@ -5,12 +5,12 @@
 #include <chrono>
 //-------------------
 //new headers required by the parallel version
-#include <omp.h>
 #include <boost/mpi.hpp>
 #include <boost/serialization/serialization.hpp>
 #include <boost/serialization/access.hpp>
 #include <boost/serialization/vector.hpp>
 #include <boost/serialization/string.hpp>
+#include <omp.h>
 
 const int THREAD_COUNT = 8;
 const int COLLECTOR_ID = THREAD_COUNT-1;
@@ -28,88 +28,55 @@ class Node{
     public:
         std::vector<Edge> in, out; 
         std::string name = "";   //unique identifier of a Node
-        unsigned long long int distance = ULLONG_MAX; //default infinity
-        Node* nearestPrev;   //default NULL
-        void fix_before_sending() const {
-            if(nearestPrev == NULL){
-                return;
-            }
-            //will give error
-            nearestPrev->in = std::vector<Edge>();
-            nearestPrev->out= std::vector<Edge>();
-            //nearestPrev->fix_before_sending();
-            nearestPrev->nearestPrev = nullptr;
-            //------------------------------ 
-            //std::cout<<"nearestPrev data ready to be serialized\n";
-        }
+        unsigned long long int distance = ULLONG_MAX; //distance from start
+        std::string previousNode = ""; //unique identifier of previous node nearest to start 
+        
         friend class boost::serialization::access;
         template <class Archive>
-        void save(Archive& ar, const unsigned int version) const {
-            fix_before_sending();
-            //std::cout<<"serialization start Node\n";
+        void serialize(Archive& ar, const unsigned int version) {
+            std::cout<<"(de/)serialization start Node\n";
             ar & in;
-            //std::cout<<"in serialization complete\n";
+            //std::cout<<"in complete\n";
             ar & out;
-            //std::cout<<"out serialization complete\n";
+            //std::cout<<"out complete\n";
             ar & name;
-            //std::cout<<"name serialization complete\n";
+            //std::cout<<"name complete\n";
             ar & distance;
-            //std::cout<<"distance serialization complete\n";
-            //the error might be that nearestPrev is changed during deserialization, 
-            //and the serialization fails
-            if(nearestPrev!=NULL){
-                nearestPrev->serialize(ar, version);
-            }else{
-                ar & nearestPrev;
-            }
-            //std::cout<<"nearestPrev serialization complete\n";
+            //std::cout<<"distance complete\n";
+            ar & previousNode;
+            std::cout<<"node (de/)serialization complete\n";
         }
-        template<class Archive>
-        void load(Archive & ar, const unsigned int version){
-            ar & in;
-            ar & out;
-            ar & name;
-            ar & distance;
-            ar & nearestPrev;
-        }
-        BOOST_SERIALIZATION_SPLIT_MEMBER()
 };
 
 class Edge{
     public:
+        //unique identifier of node that owns this edge
+        std::string parentNode="";
+        //boolean true: edge part of in vector, false: edge part of out vector
+        bool isIn=false;
+        //weight of the edge
         long long int weight = LLONG_MAX;
-        Node* n;
-        void fix_before_sending() const {
-            if(n == NULL){ //impossible
-                return;
-            }
-            //will give error
-            n->in = std::vector<Edge>();
-            n->out= std::vector<Edge>();
-            n->nearestPrev = nullptr;
-            //------------------------------
-            //std::cout<<"n data ready to be serialized\n";
-        }
+        //unique identifier of other node
+        std::string otherNode = "";
+        //distance from starting node
+        unsigned long long distance = ULLONG_MAX;
+        
         friend class boost::serialization::access;
         template <class Archive>
-        void save(Archive& ar, const unsigned int version) const {
-            fix_before_sending();
-            //std::cout<<"serialization start Edge\n";
+        void serialize(Archive& ar, const unsigned int version){
+            std::cout<<"edge (de/)serialization start\n";
             ar & weight;
-            //std::cout<<"serialized weight";
-            if(n != NULL){
-                n->serialize(ar, version);
-            }else{
-                ar & n;
-            }
-            //std::cout<<"serialization complete Edge\n";
+            std::cout<<"weight complete\t";
+            ar & distance;
+            std::cout<<"distance complete\t";
+            ar & isIn;
+            std::cout<<"isIn complete\t";
+            ar & parentNode;
+            std::cout<<"parent node complete\t";
+            ar & otherNode;
+            std::cout<<"other node complete\n";
+            std::cout<<"edge (de/)serialization complete\n";
         }
-        template <class Archive>
-        void load(Archive& ar, const unsigned int version){
-            ar & weight;
-            ar & n;
-        }
-        BOOST_SERIALIZATION_SPLIT_MEMBER()
 };
 
 void djikstra_splitter(std::vector<Node> &start, boost::mpi::communicator world){
@@ -120,77 +87,85 @@ void djikstra_splitter(std::vector<Node> &start, boost::mpi::communicator world)
         world.recv(boost::mpi::any_source, FREE_WORKER_TAG, free_worker_id);
         Node current = queue.front();    //get first node
         queue.pop(); //removes first node
-
-        //should solve serialization missing data
-        for(Node n: start){
-            if(current.name==n.name){
-                n.distance = current.distance;
-                current = n;
-                break;
-            }
-        }
-        // maybe
-        //current.fix_before_sending();
+        //current.fix_before_serializing();
         std::cout<<"assigning work to "<<free_worker_id<<"\n";
         world.send(free_worker_id, ASSIGN_WORK_TAG, &current);
         std::cout<<"in2\n";
         if(queue.empty()){
             std::cout<<"empty queue 1\n";
-            Node n;
-            //receive from collector queue containing other nodes
+            Edge e;
+            //receive from collector edge to add in the queue
             //blocking call (may create problems in the end)
-            world.recv(COLLECTOR_ID, COLLECTOR_TO_SPLITTER_TAG, n);
+            world.recv(COLLECTOR_ID, COLLECTOR_TO_SPLITTER_TAG, e);
             std::cout<<"empty queue 2\n";
-            //if n not null
             //impossible
-            if(n.name == ""){
+            if(e.otherNode == "" || e.parentNode==""){
                 std::cout<<"exit 1\n";
                 break;
             }
-            //should solve serialization missing data
-            for(Node tmp: start){
-                if(n.name==tmp.name){
-                    tmp.distance = n.distance;
-                    n = tmp;
-                    break;
+            bool done1 = false, done2 = false;
+            for(long unsigned int i = 0; i<start.size() && !(done1 && done2);++i){
+                if(start[i].name==e.otherNode){
+                    queue.push(start[i]);
+                    done1=true;
+                }else if(start[i].name==e.parentNode){
+                    if(e.isIn){
+                        for(long unsigned int j=0;j<start[i].in.size();++j){
+                            if(start[i].in[i].otherNode==e.otherNode){
+                                start[i].in[i].distance = e.distance;
+                                break;
+                            }
+                        }
+                    }else{
+                        for(long unsigned int j=0;j<start[i].out.size();++j){
+                            if(start[i].out[i].otherNode==e.otherNode){
+                                start[i].out[i].distance = e.distance;
+                                break;
+                            }
+                        }
+                    }
+                    done2=true;
                 }
             }
-            // maybe
-            queue.push(n);
+            if(!done1){
+                std::cout<<"Not pushed new node\n";
+            }
+            if(!done2){
+                std::cout<<"Not updated edge\n";
+            }
         }
     }
     std::cout<<"exit2\n";
     Node final;
     final.name = "";
     for(int worker_id = SPLITTER_ID+1; worker_id<COLLECTOR_ID;++worker_id){
-        //final.fix_before_sending();
+        //final.fix_before_serializing();
         world.isend(worker_id, ASSIGN_WORK_TAG, final);
     }
 }
 void djikstra_collector(boost::mpi::communicator world){
-    Node n;
+    Edge e;
     while(true){
-        world.recv(boost::mpi::any_source, WORKER_TO_COLLECTOR_TAG, n);
-        //n.fix_before_sending();
-        world.isend(SPLITTER_ID, COLLECTOR_TO_SPLITTER_TAG, &n);
-        if(n.name == ""){
+        world.recv(boost::mpi::any_source, WORKER_TO_COLLECTOR_TAG, e);
+        //n.fix_before_serializing();
+        world.isend(SPLITTER_ID, COLLECTOR_TO_SPLITTER_TAG, &e);
+        if(e.otherNode == "" || e.parentNode == ""){
             break;
         }
     }
 }
 void djikstra_worker(boost::mpi::communicator world){
-    Node n;
     while(true){
         //notify with potentially blocking call that worker is free
         world.send(SPLITTER_ID, FREE_WORKER_TAG, world.rank());
         std::cout<<"waiting for work rank "<<world.rank()<<"\n";
-        //TODO fix deserialization problem length error, may be vector
+        Node n;
         world.recv(SPLITTER_ID, ASSIGN_WORK_TAG, n);
         std::cout<<"got work rank "<<world.rank()<<"\n";
-        //never happens, always receive front of queue
+        //might happen only in the end
         if(n.name==""){
             if(world.rank()==COLLECTOR_ID-1){
-                //n.fix_before_sending();
+                //n.fix_before_serializing();
                 world.send(COLLECTOR_ID, WORKER_TO_COLLECTOR_TAG, &n);
             }
             break;
@@ -198,22 +173,20 @@ void djikstra_worker(boost::mpi::communicator world){
         //use open mp to parallelize this for cicle
         for(long unsigned int i = 0;i<n.out.size();++i){    //for all neighbor of the node
             unsigned long long int tmpDistance = n.distance+n.out[i].weight;
-            if(tmpDistance<n.out[i].n->distance){
-                n.out[i].n->distance=tmpDistance;
-                n.out[i].n->nearestPrev=&n;
-                //TODO fix nearestPrev might be reset
+            if(tmpDistance<n.out[i].distance){
+                n.out[i].distance=tmpDistance;
+                n.out[i].otherNode=n.name;
                 //non blocking call to send new node in queue
-                //n.out[i].n->fix_before_sending();
-                world.isend(COLLECTOR_ID, WORKER_TO_COLLECTOR_TAG, n.out[i].n);
+                //n.out[i].n->fix_before_serializing();
+                world.isend(COLLECTOR_ID, WORKER_TO_COLLECTOR_TAG, n.out[i]);
             }
         }
     }
 }
 
+
 Node generate_node(std::string name){
     Node n;
-    n.distance=LLONG_MAX;
-    n.nearestPrev = NULL;
     n.name = name;
     return n;
 }
@@ -226,10 +199,17 @@ void make_graph(std::vector<Node> &list){
             e.weight=rand()%list.size();
             long unsigned int pos = rand()%list.size();
             if(pos!=i){
-                e.n = &(list[pos]);
-                list[i].in.push_back(e);
-                e.n = &(list[i]);
-                list[pos].out.push_back(e);
+                e.parentNode = list[pos].name;
+                e.isIn = true;
+                e.otherNode = list[i].name;
+                e.distance = list[i].distance;
+                list[pos].in.push_back(e);
+                
+                e.parentNode = list[i].name;
+                e.isIn = false;
+                e.otherNode = list[pos].name;
+                e.distance = list[pos].distance;
+                list[i].out.push_back(e);
             }
         }
     }
@@ -262,15 +242,19 @@ int main(int argc, char* argv[]){
     make_graph(graph);
 
     graph[0].distance=0;
-    graph[0].nearestPrev=NULL;
     // to have at least one edge
     Edge tmp;
+    tmp.parentNode = graph[0].name;
+    tmp.isIn = false;
     tmp.weight = rand()/2;
-    //create edge exiting from first
-    tmp.n = &(graph[graph.size()-1]);
+    tmp.otherNode = graph[graph.size()-1].name;
+    tmp.distance = graph[graph.size()-1].distance;
     graph[0].out.push_back(tmp);
     //letting know last that has a edge coming from first
-    tmp.n = &(graph[0]);
+    tmp.parentNode = graph[graph.size()-1].name;
+    tmp.isIn = true;
+    tmp.otherNode = graph[0].name;
+    tmp.distance = graph[0].distance;
     graph[graph.size()-1].in.push_back(tmp);
     //-------------------------------
 
@@ -286,21 +270,19 @@ int main(int argc, char* argv[]){
                 <<"\n--------------------------------------------------\n"
                 <<"out nodes: \n";
             for(long unsigned int j = 0;j<graph[i].out.size();++j){
-                std::cout<<"\t\t"<<graph[i].out[j].weight<<" "<<graph[i].out[j].n->name<<"\n";
+                std::cout<<"\t\t"<<graph[i].out[j].weight<<" "<<graph[i].out[j].otherNode<<"\n";
             }
             std::cout<<"\n--------------------------------------------------\n";
             std::cout<<"in nodes: \n";
             for(long unsigned int j = 0;j<graph[i].in.size();++j){
-                std::cout<<"\t\t"<<graph[i].in[j].weight<<" "<<graph[i].in[j].n->name<<"\n";
+                std::cout<<"\t\t"<<graph[i].in[j].weight<<" "<<graph[i].in[j].otherNode<<"\n";
             }
             std::cout<<"\n--------------------------------------------------\n";
-        }
-        
+        }   
     }else if(world.rank()==COLLECTOR_ID){
         djikstra_collector(world);
     }else{
         djikstra_worker(world);
     }
-
     return 0;
 }
