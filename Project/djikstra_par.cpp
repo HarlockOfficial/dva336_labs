3523ab93fd5d66a9a7e929f997958e83e0566065
HarlockOfficial <harlockofficial.github@gmail.com>
2021-01-08 20:48:23 +0100

    Update djikstra_par.cpp

--- Project/djikstra_par.cpp
+++ Project/djikstra_par.cpp
@@ -70,17 +70,11 @@ class Edge{
             for(int i=0;i<20;++i){
                 stat = req.test();
                 if(stat){
-                    //std::cout<<"start if\n";
                     world.recv(stat->source(), tag+3, e.isIn);
-                    //std::cout<<"got inIn\t";
                     world.recv(stat->source(), tag+4, e.weight);
-                    //std::cout<<"got weight\t";
                     world.recv(stat->source(), tag+5, e.otherNode);
-                    //std::cout<<"got otherNode\t";
                     world.recv(stat->source(), tag+6, e.distance);
-                    //std::cout<<"got distance\t";
                     e.lastSrc = stat->source();
-                    //std::cout<<"got all\n";
                     return e;
                 }
                 std::this_thread::sleep_for(std::chrono::milliseconds(1));
@@ -95,43 +89,19 @@ class Edge{
         friend class boost::serialization::access;
         template <class Archive>
         void save(Archive& ar, const unsigned int version) const{
-            //std::cout<<"edge (de/)serialization start\n";
             ar << weight;
-            std::cout<<"weight send "<<weight<<"\n";
-            //std::cout<<"weight complete\t";
             ar << distance;
-            std::cout<<"distance send "<<distance<<"\n";
-            //std::cout<<"distance complete\t";
             ar << isIn;
-            std::cout<<"isIn send "<<isIn<<"\n";
-            //std::cout<<"isIn complete\t";
             ar << parentNode;
-            std::cout<<"parent Node send "<<parentNode<<"\n";
-            //std::cout<<"parent node complete\t";
             ar << otherNode;
-            std::cout<<"other Node send "<<otherNode<<"\n";
-            //std::cout<<"other node complete\n";
-            //std::cout<<"edge (de/)serialization complete\n";
         }
         template <class Archive>
         void load(Archive& ar, const unsigned int version){
-            //std::cout<<"edge (de/)serialization start\n";
             ar >> weight;
-            std::cout<<"weight recv "<<weight<<"\n";
-            //std::cout<<"weight complete\t";
             ar >> distance;
-            std::cout<<"distance recv "<<distance<<"\n";
-            //std::cout<<"distance complete\t";
             ar >> isIn;
-            std::cout<<"isin recv "<<isIn<<"\n";
-            //std::cout<<"isIn complete\t";
             ar >> parentNode;
-            std::cout<<"parent node recv "<<parentNode<<"\n";
-            //std::cout<<"parent node complete\t";
             ar >> otherNode;
-            std::cout<<"other node recv "<<otherNode<<"\n";
-            //std::cout<<"other node complete\n";
-            //std::cout<<"edge (de/)serialization complete\n";
         }
         BOOST_SERIALIZATION_SPLIT_MEMBER()*/
 };
@@ -194,44 +164,27 @@ class Node{
         friend class boost::serialization::access;
         template <class Archive>
         void save(Archive& ar, const unsigned int version) const{
-            //std::cout<<"serialization start Node\n";
             if(in.size()>0){
                 ar << in;
             }
             if(out.size()>0){
                 ar << out;
             }
-            //std::cout<<"send lenOut: "<<out.size()<<"\n";
-            //std::cout<<"out complete\n";
-            
-            //std::cout<<"send lenIn: "<<in.size()<<"\n";
-            //std::cout<<"in complete\n";
             ar << name;
-            //std::cout<<"name complete\n";
             ar << distance;
-            //std::cout<<"distance complete\n";
             ar << previousNode;
-            //std::cout<<"node serialization complete\n";
         }
         template <class Archive>
         void load(Archive& ar, const unsigned int version){
-            //std::cout<<"deserialization start Node\n";
             if(in.size()>0){
                 ar >> in;
             }
             if(out.size()>0){
                 ar >> out;
             }
-            //std::cout<<"received in lenOut: "<<out.size()<<"\n";
-            //std::cout<<"out complete\n";
-            //std::cout<<"received in lenIn: "<<in.size()<<"\n";
-            //std::cout<<"in complete\n";
             ar >> name;
-            //std::cout<<"name complete\n";
             ar >> distance;
-            //std::cout<<"distance complete\n";
             ar >> previousNode;
-            //std::cout<<"node serialization complete\n";
         }
         BOOST_SERIALIZATION_SPLIT_MEMBER()*/
 };
@@ -255,8 +208,6 @@ void djikstra_splitter(std::vector<Node> &start, boost::mpi::communicator world)
         world.recv(boost::mpi::any_source, FREE_WORKER_TAG, free_worker_id);
         Node current = queue.front();    //get first node
         queue.pop(); //removes first node
-        //current.fix_before_serializing();
-        //std::cout<<"assigning work to "<<free_worker_id<<"\n";
         
         current.send(world, free_worker_id, ASSIGN_WORK_TAG);
         bool done1 = false;
@@ -266,15 +217,12 @@ void djikstra_splitter(std::vector<Node> &start, boost::mpi::communicator world)
                 //all workers sent an empty node -> exiting
                 break;
             }
-            //std::cout<<"Waiting for new Edge to put in queue\n";
             e = e.irecv(world, boost::mpi::any_source, WORKER_TO_COLLECTOR_TAG);
-            //std::cout<<e.otherNode<<" "<<e.parentNode<<" "<<e.lastSrc<<"\n";
             if(strcmp(e.otherNode,"")==0 || strcmp(e.parentNode,"")==0){
                 if(e.lastSrc == -1){
                     break;
                 }
                 emptyNodeNotified.push_back(e.lastSrc);
-                //std::cout<<e.lastSrc<<" gave empty node\n";
                 continue;
             }
             for(long unsigned int i = 0; i<start.size();++i){
@@ -292,14 +240,12 @@ void djikstra_splitter(std::vector<Node> &start, boost::mpi::communicator world)
             }
         }
         if(!done1){
-            //std::cout<<"exit00\n";
             break;
         }
     }
     Node final;
     final.name = "";
     for(int worker_id = SPLITTER_ID+1; worker_id<THREAD_COUNT;++worker_id){
-        //final.fix_before_serializing();
         final.isend(world, worker_id, ASSIGN_WORK_TAG);
     }
 }
@@ -420,7 +366,6 @@ int main(int argc, char* argv[]){
     //-------------------------------
 
     if(world.rank()==SPLITTER_ID){
-        //std::cout<<"Graph solution started\n";
         auto funct_start = std::chrono::steady_clock::now();
         djikstra_splitter(graph, world);
         auto funct_end = std::chrono::steady_clock::now();
@@ -441,10 +386,8 @@ int main(int argc, char* argv[]){
             }
             std::cout<<"\n--------------------------------------------------\n";
         }
-        //std::cout<<world.rank()<<" ended\n";
     }else{
         djikstra_worker(world);
-        //std::cout<<world.rank()<<" ended\n";
     }
     return 0;
 }
