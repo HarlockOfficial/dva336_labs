f11863f92b4ab35ab3b37efce9ae7a935beeb279
Harlock <harlockofficial.github@gmail.com>
2021-02-02 16:10:40 +0100

    changed source after meet

--- Project2/pi_approximation_par.cpp
+++ Project2/pi_approximation_par.cpp
@@ -4,25 +4,33 @@
 #include <x86intrin.h>
 #include <limits>
 
-#define ALIGNMENT 16 //using sse
 #define NUM_THREADS 4
 #define ELEMENTS_IN_VECTOR 2
 
 double approximate_pi(unsigned long long int N){
-    const double dx = 1.0/(double)N;
     double return_value=0;
+    #pragma omp parallel num_threads(NUM_THREADS)
+    {
+        __m128d dx = _mm_set1_pd(1.0 / (double) N);
+        __m128d base = _mm_set_pd(0.0, 1.0);
+        __m128d sum = _mm_set1_pd(0.0);
+        __m128d one = _mm_set1_pd(1.0);
+        __m128d elements_in_vector = _mm_set1_pd((double)ELEMENTS_IN_VECTOR);
 
-    #pragma omp parallel for num_threads(NUM_THREADS)
-    for(long long unsigned int p=0;p<N/ELEMENTS_IN_VECTOR;++p) {
-        __m128d f_xi = _mm_sqrt_pd(_mm_set_pd(1 - (ELEMENTS_IN_VECTOR * p * dx * ELEMENTS_IN_VECTOR * p * dx),
-                                              1 - (ELEMENTS_IN_VECTOR * p + 1) * dx * (ELEMENTS_IN_VECTOR * p + 1) * dx));
-        __m128d tmp_results = _mm_mul_pd(_mm_set1_pd(dx), f_xi);
-
-        //now do horizontal reduce to obtain the final value
-        tmp_results = _mm_add_pd(tmp_results, _mm_move_sd(tmp_results, tmp_results));
-        double tmp = _mm_cvtsd_f64(tmp_results);
+        #pragma omp for
+        for (long long unsigned int p = 0; p < N / ELEMENTS_IN_VECTOR; ++p) {
+            __m128d i = _mm_add_pd(base, _mm_mul_pd(elements_in_vector, _mm_set1_pd((double)p)));
+            __m128d tmp_results = _mm_mul_pd(i, dx);
+            tmp_results = _mm_mul_pd(tmp_results, tmp_results);
+            tmp_results = _mm_sub_pd(one, tmp_results);
+            tmp_results = _mm_sqrt_pd(tmp_results);
+            tmp_results = _mm_mul_pd(dx, tmp_results);
+            sum = _mm_add_pd(sum, tmp_results);
+        }
+        sum = _mm_add_pd(sum, _mm_move_sd(sum, sum));
+        double tmp = _mm_cvtsd_f64(sum);
         #pragma omp atomic
-        return_value+=tmp;
+        return_value += tmp;
     }
     return return_value*4;
 }
